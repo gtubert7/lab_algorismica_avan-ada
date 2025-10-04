b87dfeb9cb767f0fe12a12a7baa87dc490fb5a94
Guillem <guillem.tubert.v@gmail.com>
2025-10-04 17:56:53 +0200

    Canvi a l'exercici 1 per fer-lo bfs

--- practica 1/exercici1.py
+++ practica 1/exercici1.py
@@ -15,17 +15,19 @@ def bfs(G, origin):
     =======
     :visited: El conjunt de nodes visitats durant l'exploració BFS en l'ordre en que han estat explorats
     """
-    visited = []
-    visited_set = set()    
-
-    bfs_aux(G, origin, visited, visited_set)
+    #Formem la llista on anirem afegint els nodes accessibles
+    visited = [origin]
+    #Formem un conjunt per a poder saber a quins nodes hem accedit amb cost O(1)
+    visited_set = set()
+    visited_set.add(origin)
+    
+    i = 0
+    #Afegim a la llista els nodes a mesura que els trobem. Els visitem també a mesura que els trobem (per aixó anem incrementant i)
+    while i < len(visited):
+        for neigh in G.neighbors(visited[i]):
+            if neigh not in visited_set:
+                visited.append(neigh)
+                visited_set.add(neigh)
+        i+= 1
 
     return visited
-
-def bfs_aux(G, node, visited, visited_set):
-    if node not in visited_set:
-        visited.append(node)
-        visited_set.add(node)
-
-        for neigh in G.neighbors(node):
-            bfs_aux(G, neigh, visited, visited_set)
