c62a25a202a5969c18410e493d280274c6ccb4fd
Guillem <guillem.tubert.v@gmail.com>
2025-10-23 10:59:45 +0200

    Començat exercici 2

---
+++ practica 2/exercici2.py
@@ -0,0 +1,67 @@
+def refill_prices_optim(K, stations, prices):
+    """
+    Repostatge òptim de vehicles amb costos. 
+    
+    Params
+    ======
+    :K: dipòsit del vehicle
+    :stations: llista de benzineres. L'últim element és el destí.
+    :prices: Llista de preus. Té un element menys que 'stations'.
+    
+    Returns
+    =======
+    :exists: Booleà True/False depenent de si existeix o no solució al problema.
+    :num_stops: Número de benzineres a les que hem de parar.
+    :stops: Quilòmetres de les benzineres on fem parada.
+    :value: Cost del trajecte.
+    """
+    
+    exists = False    
+    stops = []
+    num_stops = len(stops)
+    value = 0.0
+    prices.append(0)
+    index_estacio_actual = -1
+    estacio_actual = 0
+    stops = []
+    value = 0.0
+    litres_actuals = K
+
+    exists = True
+    while estacio_actual < stations[-1] and exists:
+        if stations[index_estacio_actual + 1] - estacio_actual > K:
+            exists = False
+        else:
+            preu_actual = prices[index_estacio_actual]
+            barata = prices[index_estacio_actual + 1]
+            i = index_estacio_actual + 2
+            index_estacio_actual += 1
+            while i < len(prices) and stations[i] - estacio_actual <= K and barata >= preu_actual:
+                if prices[i] < barata:
+                    barata = prices[i]
+                    index_estacio_actual = i
+                i += 1
+
+            if barata < preu_actual:
+                if litres_actuals < stations[index_estacio_actual] - estacio_actual:
+                    value += preu_actual*(stations[index_estacio_actual] - estacio_actual - litres_actuals)
+                    litres_actuals = 0
+                else:
+                    litres_actuals -= stations[index_estacio_actual] - estacio_actual
+            else:
+                #CAL FER AIXO
+                value += preu_actual*(K-litres_actuals)
+                    
+                    #tope = hemomplir + actual = dist = varribar - estem >>> hemomplir = varribar - estem - actual
+                    #gasprin + gasomp - disfeta = gasfinal 
+            value = round(value, 2)
+        
+        estacio_actual = stations[index_estacio_actual]
+        stops.append(estacio_actual)
+
+    num_stops = len(stops)
+
+    return exists, num_stops, stops, value
+
+    return exists, num_stops, stops, value
+        
