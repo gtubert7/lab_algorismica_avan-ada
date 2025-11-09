20a326f04a76b379ce28461f61cb915cc4d4abd6
Guillem <guillem.tubert.v@gmail.com>
2025-11-09 15:24:58 +0100

    Comentaris exercici 1

--- practica 2/exercici 1.py
+++ practica 2/exercici 1.py
@@ -21,20 +21,25 @@ def refill_prices(K, stations, prices):
     value = 0.0
     
     exists = True
+    #Revisem que no haguem arribat ja a l'estacio
     while estacio_actual < stations[-1] and exists:
+        #Si la següen estacio està més lluny que on podem arribar
         if stations[index_estacio_actual + 1] - estacio_actual > K:
             exists = False
+        #Si el desti esta a l'abast del següent trajecte
         elif stations[-1] - estacio_actual <= K:
             index_estacio_actual = -1
         else:
-            barata = prices[index_estacio_actual + 1]
-            i = index_estacio_actual + 2
+            #Primer busquem la següent estació més barata a l'abast
             index_estacio_actual += 1
+            barata = prices[index_estacio_actual]
+            i = index_estacio_actual + 1
             while i < len(prices) and stations[i] - estacio_actual <= K:
                 if prices[i] < barata:
                     barata = prices[i]
                     index_estacio_actual = i
                 i += 1
+            #Calculem el preu
             value += prices[index_estacio_actual]*(stations[index_estacio_actual] - estacio_actual)
             value = round(value, 1)
         
